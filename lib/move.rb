f5bdcb1678f604d3b5fda2a91292260c7a642812
David Cha <david.cha44@qmail.cuny.edu>
2019-10-29 20:12:21 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -16,3 +16,30 @@ def move(board, index, letter = "X")
   board[index] = letter
   display_board(board)
 end
+
+def is_position_taken?(board, index)
+  #is there already a value there? X or O?
+  #board[index] == 'X' || board[index] == or
+  if board[index] == 'X' || board[index] == 'O'
+    return true
+    else 
+    return false
+  end
+end
+
+def is_move_valid?(board, index)
+  !is_position_taken?(board, index) && index.between?(0, 8)
+end
+
+def turn(board)
+  puts "Choose a position to play on the board:"
+  user_input = gets.strip
+  board_index = input_to_index(user_input) 
+    if is_move_valid?(board, board_index)
+      move(board, board_index, "X")
+    else 
+      puts "Invalid move"
+      turn(board)
+    end
+
+end
